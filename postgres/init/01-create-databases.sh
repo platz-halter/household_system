a178a27035eb225defad93c019f5d60d42565b9c
platz-halter <simon.pressel@gmx.de>
2026-09-23 11:42:22 +0200

    Fixed database creation error

--- postgres/init/01-create-databases.sh
+++ postgres/init/01-create-databases.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 for db in auth household storage; do
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     SELECT 'CREATE DATABASE $db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
 EOSQL
 done
